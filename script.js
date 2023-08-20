722d3e017dbb6547d67686050991f69af640b3ce
pernekshetra <deekshith.bellare@icloud.com>
2023-08-20 13:54:04 +0530

    update scipt

--- script.js
+++ script.js
@@ -53,10 +53,10 @@ function generateProfilePic() {
                 userX = 0;
                 userY = (overlayImage.height - userHeight) / 2;
             } else {
-                userWidth = overlayImage.height * aspectRatio;
-                userHeight = overlayImage.height;
-                userX = (overlayImage.width - userWidth) / 2;
-                userY = 0;
+                userWidth = overlayImage.width;
+                userHeight = overlayImage.height*aspectRatio
+                userX = 0;
+                userY = (overlayImage.height - userHeight) / 2;
             }
 
             // Then draw userImage with aspect-fit
